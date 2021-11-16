7ca57241bbe7cf0f0fbb6d59ad5926b7be67013d
Greg <gregory.ceuleers@bstorm.be>
2021-11-16 20:58:57 +0100

    Add routing module Add prime config module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {IdbService} from "./db/core/idb.service";
 import {Subscription} from "rxjs";
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,36 @@ export class AppComponent implements OnInit, OnDestroy{
   title = 'Wist2021';
 
   dataPlayersSubscription: Subscription | undefined;
+  items: MenuItem[] = [
+    {
+      label: 'Accueil',
+      icon: 'pi pi-fw pi-home',
+      routerLink: ''
+    },
+    {
+      label: 'Parties',
+      icon: 'pi pi-fw pi-heart',
+      items: [
+        {
+          label: 'Nouvelle Partie',
+          icon: 'pi pi-fw pi-plus',
+        },
+        {
+          label: 'Tableau',
+          icon: 'pi pi-fw pi-table'
+        }
+      ]
+    },
+    {
+      label: 'Edit',
+      icon: 'pi pi-fw pi-pencil',
+      items: [
+        {label: 'Delete', icon: 'pi pi-fw pi-trash'},
+        {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+      ]
+    }
+
+  ];
 
   constructor(
     private dbService: IdbService
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {MenubarModule} from 'primeng/menubar'
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
+import { PrimeConfigModule } from './prime/prime-config/prime-config.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    PrimeConfigModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
