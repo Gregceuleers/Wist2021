251286fc4503efd63008bf77bea170d05467a5b2
Greg <gregory.ceuleers@bstorm.be>
2021-12-30 16:33:59 +0100

    Fix local french

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
+import {CommonModule, registerLocaleData} from '@angular/common';
+import {LOCALE_ID, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import {AppRoutingModule} from './app-routing/app-routing.module';
@@ -9,13 +9,16 @@ import {HomeComponent} from './home/home.component';
 import {PrimeConfigModule} from './prime/prime-config/prime-config.module';
 import {PartieComponent} from './partie/partie.component';
 import {MessageService} from "primeng/api";
-import { SelectionJoueursComponent } from './partie/selection-joueurs/selection-joueurs.component';
+import {SelectionJoueursComponent} from './partie/selection-joueurs/selection-joueurs.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {InputTextModule} from "primeng/inputtext";
-import { NombrePartiesComponent } from './partie/nombre-parties/nombre-parties.component';
-import { ConfirmationPartieComponent } from './partie/confirmation-partie/confirmation-partie.component';
-import { PartieEnCoursComponent } from './partie/partie-en-cours/partie-en-cours.component';
+import {NombrePartiesComponent} from './partie/nombre-parties/nombre-parties.component';
+import {ConfirmationPartieComponent} from './partie/confirmation-partie/confirmation-partie.component';
+import {PartieEnCoursComponent} from './partie/partie-en-cours/partie-en-cours.component';
 import {HttpClientModule} from "@angular/common/http";
+import localeFr from '@angular/common/locales/fr';
+
+registerLocaleData(localeFr, 'fr');
 
 @NgModule({
   declarations: [
@@ -38,7 +41,13 @@ import {HttpClientModule} from "@angular/common/http";
     HttpClientModule,
     InputTextModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'fr-FR'
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
